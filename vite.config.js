8399a3be972544ad4a3a74b2466bba6d656063bb
justonelook101 <justonelook101@gmail.com>
2025-11-26 13:41:33 +0545

    Fix: Use default esbuild minifier instead of terser

--- vite.config.js
+++ vite.config.js
@@ -13,12 +13,5 @@ export default defineConfig({
         },
       },
     },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
   },
 })
